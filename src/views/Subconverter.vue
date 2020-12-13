8da85ef8711626633e59979cd5971417325b54f7
Tycoon <tycoon2151@gmail.com>
2020-12-13 22:49:34 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -715,8 +715,7 @@ export default {
           callback: action => {
             this.$message({
               type: 'error',
-              /* message: `action: ${ action }` */
-              message: '╮(╯▽╰)╭ 真是没办法呢'
+              message: `╮(╯▽╰)╭ 真是没办法呢`
             });
           }
         });
