c8f8f8a2614aa1fa0b643318e865b90ddac36638
Tycoon <tycoon2151@gmail.com>
2020-12-19 23:29:09 +0800

    update

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -275,7 +275,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          hosts: hostsd,
+          [hosts]: hostsd,
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
